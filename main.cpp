74d3c3bfaa93ff7b645f11b3f953130de0434238
meats88 <pcx883mi@gmail.com>
2026-06-02 18:09:32 -0700

    done

--- main.cpp
+++ main.cpp
@@ -6,6 +6,16 @@ int main()
 {
 	int prev, current;
 	// TODO
+	int flag = 1;
+	cin >> current;
+	while ((flag == 1) or ( prev >= current)){
+		flag = 0;
+		cout << current << "\t";
+		prev = current;
+		cin >> current;
+	}
 
 	// TODO
+	cout << endl;
+	return 0;
 }
